c7431a6a7baf2d2e62f2165868af3cf8fdebb080
eudora-hsj <eudora.hsj@gmail.com>
2020-08-19 18:13:25 +0800

    add axios / bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,142 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"></script>
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+    <div class="container my-3" id="app">
+        <div class="input-group mb-3">
+            <div class="input-group-prepend"><span class="input-group-text" id="basic-addon1">待辦事項</span></div>
+            <input class="form-control" type="text" placeholder="準備要做的任務" v-model="newTodo" @keyup.enter="addTodo"/>
+            <div class="input-group-append">
+            <button class="btn btn-primary" type="button" @click="addTodo">新增</button>
+            </div>
+        </div>
+        <div class="card text-center">
+            <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs">
+                <li class="nav-item"><a class="nav-link" href="#" :class=" {'active' : visibility == 'all' } " @click="visibility='all'">全部</a></li>
+                <li class="nav-item"><a class="nav-link" href="#" :class="{'active' : visibility == 'active'}" @click="visibility='active'">進行中</a></li>
+                <li class="nav-item"><a class="nav-link" href="#" :class="{'active' : visibility == 'completed'}" @click="visibility='completed'">已完成</a></li>
+            </ul>
+            </div>
+            <ul class="list-group list-group-flush text-left">
+            <li class="list-group-item" v-for="item in filteredTodos" @dblclick="editTodo(item)">
+                <div class="d-flex" v-if="item.id !== cacheTodo.id">
+                <div class="form-check">
+                    <input class="form-check-input" :id="item.id" type="checkbox" v-model="item.completed"/>
+                    <label class="form-check-label" :for="item.id" :class="{'completed':item.completed}">{{ item.title }}</label>
+                </div>
+                <button class="close ml-auto" type="button" aria-label="Close" @click="removeTodo(item)"><span aria-hidden="true">×</span></button>
+                </div>
+                <input class="form-control" type="text" v-model="cacheTitle" v-if="item.id == cacheTodo.id" @keyup.esc="cancelEdit" @keyup.enter="doneEdit(item)"/>
+            </li>
+            </ul>
+            <div class="card-footer d-flex justify-content-between">
+            <section>
+                還有  <span>{{activeTodosLength}}</span> 筆任務未完成</section><a href="#" @click="cleanTodo">清除所有任務</a>
+            </div>
+        </div>
+        </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    data() {
+        return {
+            newTodo:'',
+            todos: 
+            [
+                {
+                    id: '0',
+                    title: 'hello',
+                    completed: false
+                }
+            ],
+            
+            visibility: 'all',
+            cacheTodo: {},
+            cacheTitle: ''
+
+            }
+    },
+    created: function () {
+        this.getApi("https://eudora-hsj.github.io/Vue-practice/data/todolist.json");
+    }, 
+    methods: {
+        getApi: function(url){
+            axios.get(url)
+            .then((res) =>{
+                this.todos = res.data.data
+            })
+            .catch((err)=>{
+                console.log(err)
+            })
+        },
+        addTodo: function(){
+            var value = this.newTodo.trim()
+            var timestamp = Math.floor(Date.now())
+            if ( !value ) { return }
+            this.todos.push(
+                {
+                    id: timestamp,
+                    title: value,
+                    computed: false
+                }
+            ),
+            this.newTodo = ''
+        },
+        removeTodo: function(todo){
+            var newIndex = this.todos.findIndex(function(item){
+                return todo.id === item.id
+            })
+            this.todos.splice(newIndex,1)
+          },
+        editTodo: function(item){
+            this.cacheTodo = item
+            this.cacheTitle = item.title
+        },
+        cancelEdit: function(){
+            this.cacheTodo = {}
+        },
+        doneEdit: function(item){
+            item.title = this.cacheTitle
+            this.cacheTitle = ''
+            this.cacheTodo = {}
+        },
+        cleanTodo: function(todo){
+            allTodo = this.todos
+            allTodo.splice(0,allTodo.length)
+          },
+    },
+    computed: {
+        filteredTodos: function(){
+            var nowTab = this.visibility
+            var showList = []
+            switch (nowTab) {
+                case 'all':
+                    showList = this.todos
+                    break
+                case 'active':
+                    this.todos.forEach(function(item){
+                        if ( !item.completed ) { showList.push(item) }
+                    })
+                    break
+                case 'completed':
+                    this.todos.forEach(function(item){
+                        if ( item.completed ) { showList.push(item) }
+                    })
+            }
+            return showList
+        },
+        activeTodosLength: function(){
+            return this.todos.filter(item => !item.completed ).length
+            // var sum = 0
+            // this.todos.forEach(function(item){
+            //     if ( !item.completed ) { sum ++ }
+            // })
+            // return sum
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import 'bootstrap'; // Import js file
+import 'bootstrap/dist/css/bootstrap.min.css'; // Import css file
+window.axios = require('axios');  // https://stackoverflow.com/questions/51374367/axios-is-not-defined-in-vue-js-cli
+import axios from 'axios'
 
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
